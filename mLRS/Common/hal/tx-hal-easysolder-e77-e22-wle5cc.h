24a198312796e06f76afe204577432984ad722d5
cesforchina <1972343026@qq.com>
2023-09-08 01:19:09 +0800

    some mix

--- mLRS/Common/hal/tx-hal-easysolder-e77-e22-wle5cc.h
+++ mLRS/Common/hal/tx-hal-easysolder-e77-e22-wle5cc.h
@@ -17,6 +17,8 @@
 // TX DIY "easy-to-solder" E77 E22 dual, STM32WLE5CC
 //-------------------------------------------------------
 
+//#define DEVICE_HAS_IN
+#define DEVICE_HAS_JRPIN5
 #define DEVICE_HAS_IN_ON_JRPIN5_TX
 #define DEVICE_HAS_SERIAL_OR_COM // serial or COM is selected by pressing BUTTON during power on
 #define DEVICE_HAS_DEBUG_SWUART
@@ -57,7 +59,7 @@
 // UARTF = --
 // SWUART= debug port
 
-#define UARTB_USE_UART2 // serial // PA2,PA3
+#define UARTB_USE_UART1_REMAPPED // serial // PB6,PB7
 #define UARTB_BAUD                TX_SERIAL_BAUDRATE
 #define UARTB_USE_TX
 #define UARTB_TXBUFSIZE           TX_SERIAL_TXBUFSIZE
@@ -65,15 +67,15 @@
 #define UARTB_USE_RX
 #define UARTB_RXBUFSIZE           TX_SERIAL_RXBUFSIZE
 
-#define UARTC_USE_UART1_REMAPPED // com USB/CLI // PB6,PB7
-#define UARTC_BAUD                TX_COM_BAUDRATE
-#define UARTC_USE_TX
-#define UARTC_TXBUFSIZE           TX_COM_TXBUFSIZE
-#define UARTC_USE_TX_ISR
-#define UARTC_USE_RX
-#define UARTC_RXBUFSIZE           TX_COM_RXBUFSIZE
+//#define UARTB_USE_UART1_REMAPPED // com USB/CLI // PB6,PB7
+//#define UARTB_BAUD                TX_COM_BAUDRATE
+//#define UARTB_USE_TX
+//#define UARTB_TXBUFSIZE           TX_COM_TXBUFSIZE
+//#define UARTB_USE_TX_ISR
+//#define UARTB_USE_RX
+//#define UARTB_RXBUFSIZE           TX_COM_RXBUFSIZE
 
-#define UART_USE_UART2 // JR pin5, MBridge // PA2
+#define UART_USE_UART2 // JR pin5, MBridge // PA2,PA3
 #define UART_BAUD                 400000
 #define UART_USE_TX
 #define UART_TXBUFSIZE            512
@@ -81,11 +83,11 @@
 #define UART_USE_RX
 #define UART_RXBUFSIZE            512
 
-#ifndef MLRS_DEV_FEATURE_JRPIN5_SDIODE
-#define JRPIN5_FULL_INTERNAL_ON_TX // does not require an external diode
-#else
-#define JRPIN5_RX_TX_INVERT_SWAP_INTERNAL // requires external diode from Tx to Rx
-#endif
+//#ifndef MLRS_DEV_FEATURE_JRPIN5_SDIODE
+//#define JRPIN5_FULL_INTERNAL_ON_TX // does not require an external diode
+//#else
+//#define JRPIN5_RX_TX_INVERT_SWAP_INTERNAL // requires external diode from Tx to Rx
+//#endif
 
 #define UARTE_USE_UART2 // in port
 #define UARTE_BAUD                100000 // SBus normal baud rate, is being set later anyhow
@@ -96,7 +98,7 @@
 #define UARTE_RXBUFSIZE           512
 
 #define SWUART_USE_TIM17 // debug
-#define SWUART_TX_IO              IO_PA9 // STx pad on board
+#define SWUART_TX_IO              IO_PA8 // STx pad on board
 #define SWUART_BAUD               115200
 #define SWUART_USE_TX
 #define SWUART_TXBUFSIZE          512
@@ -314,6 +316,7 @@ void led_red_toggle(void) { gpio_toggle(LED_RED); }
 void pos_switch_init(void)
 {
 }
+
 uint8_t pos_switch_read(void)
 {
     return 0;
